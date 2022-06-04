918862b77f3676e3e6da5cd97a77affaa7c63161
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-06-04 20:02:55 +0700

    N Queens Problem (Backtracking)

---
+++ N-Queens.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> nQueens(n,string(n,'.'));
+        
+        helper(res,nQueens,0,n);
+        return res;
+    }
+    
+    void helper(vector<vector<string> > &res, vector<string> &nQueens, int row, int &n) {
+        if (row == n) {
+            res.push_back(nQueens);
+            return;
+        }
+        for (int col = 0; col != n; ++col)
+            if (isSafe(nQueens, row, col, n)) {
+                nQueens[row][col] = 'Q';
+                helper(res, nQueens, row + 1, n);
+                nQueens[row][col] = '.';
+            }
+    }
+    
+    bool isSafe(vector<string> &nQueens, int row, int col, int &n) {
+        for (int i = 0; i != row; ++i)
+            if (nQueens[i][col] == 'Q')
+                return false;
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
+            if (nQueens[i][j] == 'Q')
+                return false;
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
+            if (nQueens[i][j] == 'Q')
+                return false;
+        return true;
+    }
+};
