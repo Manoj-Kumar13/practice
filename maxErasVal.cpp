54fef641e54766f382ff511e37cb077fd5038a9c
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-06-12 13:51:51 +0700

    Maximum sum of a subarray of unique elements

---
+++ maxErasVal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> p(n+1,0);
+        
+        for(int i=0; i<n; i++){
+            p[i+1] = p[i]+nums[i];
+        }
+        
+        int s = 0;
+        int ans = 0;
+        vector<int> m(10001,-1);
+        for(int i=0; i<n; i++){
+            s = max(s,m[nums[i]]+1);
+            ans = max(ans,p[i+1]-p[s]);
+            m[nums[i]] = i; 
+        }
+        
+        return ans;
+    }
+};
