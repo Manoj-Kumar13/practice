4eba06fb87ebb7fbf049a8c4383adbd8503b9bbb
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-06-13 22:30:04 +0700

    Update minCostClimbingStairs.cpp

--- minCostClimbingStairs.cpp
+++ minCostClimbingStairs.cpp
@@ -1,3 +1,4 @@
+//memoized solution
 class Solution {
 public:
     int solve(vector<int>& cost,int n, vector<int>& dp){
@@ -22,5 +23,24 @@ public:
         vector<int> dp(n+1,-1);
         
         return min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+    }
+};
+
+//Tabulation Solution
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n= cost.size();
+        
+        vector<int> dp(n+1);
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i=2; i<n; i++){
+            dp[i] = min(dp[i-1],dp[i-2]) + cost[i];
+        }
+        
+        return min(dp[n-1],dp[n-2]);
     }
 };
