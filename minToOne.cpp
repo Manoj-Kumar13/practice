8d3a22745dd3a05e8b3690a3aba6dbd8f227ca3f
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-06-14 00:18:43 +0700

    Added Bottom-up(tabulation) approach

--- minToOne.cpp
+++ minToOne.cpp
@@ -1,3 +1,4 @@
+//With Memoization
 int minToOne(int n,vector<int>& a){
 	  if(n<=1){
         return 0;
@@ -23,6 +24,24 @@ int minToOne(int n,vector<int>& a){
     return a[n];	
 
 }
+
+//With Tabulation
+int minToOneDP(int n, vector<int>& dp){
+	if(n<=1){
+		return dp[n];
+	}
+
+	for(int i=2; i<=n; i++){
+		int x = i-1, y = i-1, z = i-1;
+		if(i%2 == 0) {y = i/2;}
+		if(i%3 == 0) {z = i/3;}
+
+		dp[i] = min(dp[x],min(dp[y],dp[z]))+1;
+	}
+
+	return dp[n];
+}
+
 void solve(){
 	int n;
 	cin >> n;
