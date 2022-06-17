137e7f56d9302617feb06decf5649e9947fb5382
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-06-17 16:15:50 +0700

    Generate all combinations of balanced parentheses

---
+++ generateParentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        helper(ans,"",n,0);
+        return ans;
+    }
+    
+    void helper(vector<string>& ans, string s, int n,int m){
+        if(n==0 && m==0){
+            ans.push_back(s);
+            return;
+        }
+        
+        if(n>0){
+            helper(ans,s + "(",n-1,m+1);
+        }
+        if(m>0){
+            helper(ans,s + ")",n,m-1);
+        }
+    }
+};
