21ea79eca64cb0d5a5a30f4d1d9107d6f133594d
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-06-20 00:32:13 +0700

    Codechef Lunchtime 19 june 2022

---
+++ DetermineScore.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include <string>
+using namespace std;
+#define int long long int
+
+void solve(){
+	int a,b;
+
+	cin >>  a >> b;
+
+	cout << (a/10)*b;
+}
+
+
+int32_t main(){
+
+	int t;
+	cin >> t;
+	while(t--){
+		solve();
+		cout << "\n";
+	}
+	
+	return 0;
+}
