e2c39122c2add294431fd74e6a18a9ab21481268
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-07-28 20:49:19 +0700

    Average value of the nodes on each level of a binary tree

---
+++ avgLevelBT.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans;
+          if(!root){
+            return ans;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            long sum = 0;
+            int s = q.size();
+            for(int i=0; i<s; i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                if(temp->left){
+                    q.push(temp->left);
+                }
+                if(temp->right){
+                    q.push(temp->right);
+                }
+                sum += temp->val;
+            }
+            ans.push_back((double)sum/s);
+        }
+        return ans;
+    }
+};
