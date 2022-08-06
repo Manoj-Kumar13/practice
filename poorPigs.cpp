88eab5085201443e14a43004cc22c27f8e18b0dc
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-08-06 16:08:51 +0700

    return minimum no. of pigs to find poisonous bucket

---
+++ poorPigs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int ans = 0;
+        
+        while(pow(minutesToTest/minutesToDie +1 ,ans) < buckets){
+            ans += 1;
+        }
+        
+        return ans;
+    }
+};
