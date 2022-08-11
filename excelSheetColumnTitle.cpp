0d2d3236e3086ee8f2f37a87e8ded649e8c424b7
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-08-12 03:05:56 +0700

    get corresponding Excel Sheet Column Title to no.

---
+++ excelSheetColumnTitle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string ans;
+        
+        char temp;
+        
+        while(n>0){
+            n-=1;
+            
+            temp = 'A' +n%26;
+            
+            ans = temp+ans;
+            
+            n = n/26;
+        }
+        
+        return ans;
+    }
+};
