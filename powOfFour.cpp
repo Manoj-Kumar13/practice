13ca4f1f292884fac2d18f794d2f1bd3563d906a
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-08-22 21:17:36 +0700

    Check if a number is a power of 4

---
+++ powOfFour.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+    bool isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
+    }
+};
