5cf7f5baaabce6de438d9b68b56acb81686b6279
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-08-23 18:54:38 +0700

    topological sort in DAG

--- topoSort.cpp
+++ topoSort.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+
+//DFS Appraoch
 void topoSort(int node,vector<bool> &visited,stack<int> &s,unordered_map<int,list<int>> &adjList){
     visited[node] = true;
     
@@ -38,5 +40,56 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
     
     return ans;
     
+    
+}
+
+
+//BFS appraoch(Kahn's algorithm)
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Write your code here
+    unordered_map<int,list<int>> adjList;
+    for(int i=0; i<edges.size();i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        
+        adjList[u].push_back(v);
+    }
+    
+    //find indegree of elements
+    vector<int> indegree(v);
+    for(auto i: adjList){
+        for(auto j: i.second){
+                indegree[j]++;    
+        }
+    }
+    
+    queue<int> q;
+    
+    //push elements with 0 indegree
+    for(int i=0; i<v;i++){
+        if(indegree[i] == 0){
+            q.push(i);
+        }
+    }
+    
+    vector<int> ans;
+    
+    //do BFS
+    while(!q.empty()){
+        int frontNode = q.front();
+        q.pop();
+        
+        //store answer
+        ans.push_back(frontNode);
+        
+        for(auto i: adjList[frontNode]){
+            indegree[i]--;
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+    }
+    
+    return ans;
     
 }
