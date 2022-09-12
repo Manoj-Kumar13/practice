c7f4e1bbaa166dc852db14730b7838946a766874
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-09-12 23:54:29 +0700

    compute and return square root of X

--- sqrtX.cpp
+++ sqrtX.cpp
@@ -8,3 +8,27 @@ public:
         return ans;
     }
 };
+
+//optimized more approach
+
+int sqrtN(long long int N)
+{
+    // Write your code here.
+     if(N == 0 || N == 1)
+    {
+        return N;
+    }
+    long long left = 2;
+    long long right = N;
+    
+    while(left<=right){
+        long long mid = left + (right-left)/2;
+        
+        if(mid <= (N/mid)){
+            left = mid+1;
+        }else{
+            right = mid-1;
+        }
+    }
+    return left-1;
+}
