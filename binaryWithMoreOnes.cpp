3beff3fbc34a6a7c0987375507b609acc724fa22
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-09-20 04:13:55 +0700

    find all N-Bit binary no. with ones greater than zeroes for all prefixes

--- binaryWithMoreOnes.cpp
+++ binaryWithMoreOnes.cpp
@@ -1,1 +1,22 @@
+void subsets(int n, int ones, int zeros, string output){
+			if(output.size() == n){
+				cout << output << endl;
+				return;
+			}
 
+			string op1 = output + "1";
+			subsets(n , ones+1, zeros, op1);
+
+			if(ones > zeros){
+				string op2 = output + "0";
+				subsets(n,ones, zeros+1,op2);
+			}
+
+		}
+
+		void solve() {
+			int n;
+			cin >> n;
+
+			subsets(n,1,0, "1");
+		}
