ed38b4e0d255b5a8404ed2576c4b8a5a9d739159
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-10-01 01:55:11 +0700

    return true if N is a power of two

--- powerOfTwo.cpp
+++ powerOfTwo.cpp
@@ -4,3 +4,14 @@ public:
         return n>0 and (n&(n-1)) == 0;
     }
 };
+
+//recursive approach
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if( n == 1){
+            return true;
+        }
+        return n>1 and n%2 == 0 and isPowerOfTwo(n/2);
+    }
+};
