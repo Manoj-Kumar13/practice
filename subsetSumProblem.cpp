ef6956c93a52d9ca8efb8179ecc7c8ded5d9674d
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-10-02 18:47:17 +0700

    determine if there is a subset with sum equal to given sum

--- subsetSumProblem.cpp
+++ subsetSumProblem.cpp
@@ -39,3 +39,54 @@ bool isPossible(vector<int>arr, int sum, int n){
         return dp[n][sum];
         // return isPossible(arr,sum, n);
     }
+
+//memoized solution
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    
+  public:
+  bool isPossible(int *arr, int sum, int n, vector<vector<int>> &dp){
+        if(n == 0){
+            if(sum == 0){
+                return true;
+            }
+            return false;
+        }
+        
+        if(dp[n][sum] != -1){
+            return dp[n][sum];
+        }
+        
+        if(arr[n-1] <= sum){
+            return dp[n][sum] = isPossible(arr,sum-arr[n-1], n-1,dp) or isPossible(arr,sum, n-1,dp);
+        }else{
+            return dp[n][sum] = isPossible(arr,sum, n-1,dp);
+        }
+    }
+    
+	int minDifference(int arr[], int n)  { 
+	    // Your code goes here
+	    int range = 0;
+	    for(int i=0; i<n; i++){
+	        range += arr[i];
+	    }
+	    vector<vector<int>> dp(n+1,vector<int>(range+1,-1));
+	    vector<int> rangeArr;
+	    for(int i=1; i<=range; i++){
+	        if(isPossible(arr,i,n,dp)){
+	            rangeArr.push_back(i);
+	        }
+	    }
+	    
+	    int ans = INT_MAX;
+	    
+	    for(int i=0; i<rangeArr.size(); i++){
+	        ans  = min(ans, abs(rangeArr[i] - (range - rangeArr[i])));
+	    }
+	    
+	    return ans;
+	}
+};
