690522d0ee4488dbaabfc2e6bf8911b038ae04ff
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-10-22 21:15:29 +0700

    No. of paths to reach bottom-right corner with obstacles

--- pathsWithObstacles.cpp
+++ pathsWithObstacles.cpp
@@ -16,3 +16,48 @@ public:
         return dp[n][m];
     }
 };
+
+
+//another approach
+void solve() {
+			int n;
+			cin >> n;
+
+			char grid[n+1][n+1];
+
+			for (int i = 1; i <= n; ++i)
+			{
+				for (int j = 1; j <= n; ++j)
+				{
+					cin >> grid[i][j];
+				}
+			}
+
+			if(grid[n][n] == '*'){
+				cout << 0;
+				return;
+			}
+
+			int dp[n+1][n+1];
+
+			for (int i = n; i>=1; i--)
+			{
+				for (int j = n; j>=1; j--)
+				{
+					if(i == n and j==n){
+						dp[i][j] = 1;
+					}else{
+						int op1 = (j == n) ? 0 : dp[i][j+1]%Mod;
+						int op2 = (i == n) ? 0 : dp[i+1][j]%Mod;
+
+						dp[i][j] = (op1+op2)%Mod;
+						if(grid[i][j] == '*'){
+							dp[i][j] = 0;
+						}
+					}
+
+				}
+			}
+
+			cout << dp[1][1];
+		}
