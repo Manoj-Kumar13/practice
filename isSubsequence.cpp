ea85dde98d3f35f8537ee7afe181ad7422543c57
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-11-02 22:16:47 +0700

    Return true if s1 is a subsequence of s2

--- isSubsequence.cpp
+++ isSubsequence.cpp
@@ -37,6 +37,31 @@ class Solution {
         return dp[i][j] = isSubSequence(s1, s2, i, j - 1,dp);
     }
 }
+    
+// bottom-up DP
+     bool isSubsequence(string a, string b) {
+        int n = a.size();
+        int m = b.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0 or j==0){
+                    dp[i][j] = 0;
+                }else if(a[i-1] == b[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        int len = dp[n][m];
+        
+        return (len == n) ? true: false;
+        
+    }
  
 public:
     bool isSubsequence(string s, string t) {
