d6c64bbc88448cb6d48372966a6bd8799570ede1
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-11-05 12:40:49 +0700

    return min no. of attempts to find threshold floor

--- eggDroppingProblem.cpp
+++ eggDroppingProblem.cpp
@@ -33,3 +33,39 @@ class Solution
         return dp[e][n] = ans;
     }
 };
+
+//optimised approach (using binary with memoization)
+class Solution {
+public:
+  int find(int e,int n,vector<vector<int>> &memo)
+    {   if(n==0||n==1) return n; 
+        if(e==1) return n;    
+        if(memo[e][n]!=-1) return memo[e][n];
+        int ans=1000000,l=1,h=n,temp=0;
+     
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            int left=find(e-1,mid-1,memo);  
+            int right=find(e,n-mid,memo) ; 
+            temp=1+max(left,right);        
+            if(left<right){              
+              l=mid+1;                    
+            }
+            else                          
+            {    
+                h=mid-1;
+            }
+            ans=min(ans,temp);
+        }
+        
+        return memo[e][n]=ans;
+     
+    }
+    int superEggDrop(int e, int n) {
+
+        vector<vector<int>> memo(e+1,vector<int> (n+1,-1));
+        return find(e,n,memo);
+         
+    }
+};
