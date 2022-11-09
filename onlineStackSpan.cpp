9a54b3af791db5976893da91add6a53db1fb2e8d
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2022-11-09 23:46:21 +0700

    Returns span of stock's price given today's price

--- onlineStackSpan.cpp
+++ onlineStackSpan.cpp
@@ -30,6 +30,41 @@ public:
     }
 };
 
+//another appraoch
+class StockSpanner {
+public:
+    stack<pair<int,int>> s;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        if(s.empty() or s.top().first > price){
+            s.push({price,1});
+            return 1;
+        }
+        
+        int span = 1;
+        
+        while(!s.empty()){
+            if(s.top().first <= price){
+                span += s.top().second;
+                s.pop();
+            }else{
+                break;
+            }
+        }
+        s.push({price,span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
+
 /**
  * Your StockSpanner object will be instantiated and called as such:
  * StockSpanner* obj = new StockSpanner();
